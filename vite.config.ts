c74e8f6c287f86312b51cbb7818894b1c1d3c4bd
Krsna Jeeva <krsnajeeva98@gmail.com>
2026-07-02 00:38:22 +0530

    Add local gh-pages deploy scripts and update dynamic base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/FinancialManagerWeb/' : '/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/FinancialManagerWeb/' : '/',
+  }
 })
